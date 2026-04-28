c346799d59c46f215c008f93bf4d9557b30938b5
SandhurstCC <bstinton@gmail.com>
2026-04-28 16:02:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sandhurst-cc-v2";
+const CACHE = "sandhurst-cc-v3";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
